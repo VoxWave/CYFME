14efc3d36d1b1e5dae36ae746b89e25498892229
Victor Bankowski <victor.bankowski@hotmail.com>
2014-07-12 19:22:53 +0300

    created the gradle project.

---
+++ CYFME/desktop/src/com/unknownpotato/cyfme/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.unknownpotato.cyfme.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unknownpotato.cyfme.cyfme;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new cyfme(), config);
+	}
+}
