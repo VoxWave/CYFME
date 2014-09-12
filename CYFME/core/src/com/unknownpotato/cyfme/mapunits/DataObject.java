d03105787138585c84b3f4260017b7c39e7066e9
Victor Bankowski <victor.bankowski@cs.helsinki.fi>
2014-09-12 11:39:03 +0300

    Commit

--- CYFME/core/src/com/unknownpotato/cyfme/mapunits/DataObject.java
+++ CYFME/core/src/com/unknownpotato/cyfme/mapunits/DataObject.java
@@ -4,9 +4,18 @@ import com.badlogic.gdx.math.Vector2;
 
 public class DataObject {
 	
-	private Vector2 pos;
-	private String type;
 	private float rot;
+	
+	/**
+	 * What we spawn
+	 */
+	private String type;
+	
+	/**
+	 * where we spawn it
+	 */
+	private Vector2 pos;
+	
 	
 	public DataObject(Vector2 pos, String type, float rot){
 		this.pos = pos;
@@ -24,6 +33,18 @@ public class DataObject {
 	
 	public String getType(){
 		return this.type;
+	}
+	
+	public void setPos(Vector2 pos){
+		this.pos=pos;
+	}
+	
+	public void setRot(float rot){
+		this.rot=rot;
+	}
+	
+	public void setType(String type){
+		this.type=type;
 	}
 	
 }
--- CYFME/core/src/com/unknownpotato/cyfme/mapunits/DataObjectSet.java
+++ CYFME/core/src/com/unknownpotato/cyfme/mapunits/DataObjectSet.java
@@ -1,9 +1,95 @@
 package com.unknownpotato.cyfme.mapunits;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
-public class DataObjectSet {
+public class DataObjectSet implements Set<DataObject> {
 	
-	private HashSet<DataObject> dataset;
+	private Set<DataObject> dataset;
+	
+	public DataObjectSet(){
+		this.dataset = new HashSet<DataObject>();
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return this.dataset.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return this.dataset.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return this.dataset.contains(o);
+	}
+
+	@Override
+	public Iterator<DataObject> iterator() {
+		// TODO Auto-generated method stub
+		return this.dataset.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return this.dataset.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return this.dataset.toArray(a);
+	}
+
+	@Override
+	public boolean add(DataObject e) {
+		// TODO Auto-generated method stub
+		return this.dataset.add(e);
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return this.dataset.remove(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return this.dataset.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends DataObject> c) {
+		// TODO Auto-generated method stub
+		return this.dataset.addAll(c);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return this.dataset.retainAll(c);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return this.dataset.removeAll(c);
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		this.dataset.clear();
+		
+	}
 
 }
