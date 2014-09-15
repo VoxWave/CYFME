383ed56596338f29cf5ff9ad2713ee726a92936b
Mikko Kumara <mikko@1bqqh8j.kumpula.dhcp.hupnet.helsinki.fi>
2014-09-15 17:58:55 +0300

    uusi main, parempi gitignore

--- CYFME/core/src/com/unknownpotato/cyfme/cyfme.java
+++ CYFME/core/src/com/unknownpotato/cyfme/cyfme.java
@@ -1,12 +1,12 @@
 package com.unknownpotato.cyfme;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class cyfme extends ApplicationAdapter {
+public class cyfme implements ApplicationListener {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -23,5 +23,29 @@ public class cyfme extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
 	}
 }
